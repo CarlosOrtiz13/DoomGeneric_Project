b3f625272abf8048eb711c1e1baeb41193a9ab5e
Brodie <bawatson789@gmail.com>
2025-12-11 18:09:45 -0500

    Enhance comments in disk_stream structure and functions
    Added comments to clarify the structure and functions.

--- src/disk/streamer.h
+++ src/disk/streamer.h
@@ -2,16 +2,19 @@
 #define DISKSTREAMER_H
 
 #include "disk.h"
-
+/*simple stream abstraction for reading from a disk*/
 struct disk_stream
 {
-    int pos;
-    struct disk* disk;
+    int pos; /*current position in bytes*/
+    struct disk* disk; /*disk this stream reads from*/
 };
-
+/*create a new stream for the given disk*/
 struct disk_stream* diskstreamer_new(int disk_id);
+/*set the current position of the stream*/
 int diskstreamer_seek(struct disk_stream* stream, int pos);
+/*read bytes from the disk into the output buffer*/
 int diskstreamer_read(struct disk_stream* stream, void* out, int total);
+/*close the stream and free its memory*/
 void diskstreamer_close(struct disk_stream* stream);
 
 #endif
