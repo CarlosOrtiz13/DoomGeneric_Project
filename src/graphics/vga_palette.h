cf682eb6797a13242d92d36a7ba36831c056ffaf
ndmitrie <142837171+ndmitrie@users.noreply.github.com>
2025-12-12 02:29:57 -0500

    Update dvga_palette.h

--- src/graphics/vga_palette.h
+++ src/graphics/vga_palette.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-void vga_set_palette_entry(uint8_t index, uint8_t r, uint8_t g, uint8_t b);
-void vga_set_doom_palette(const uint8_t* palette);
-void vga_set_default_palette(void);
+void vga_set_palette_entry(uint8_t index, uint8_t r, uint8_t g, uint8_t b);    /*Set a single VGA palette entry*/
+void vga_set_doom_palette(const uint8_t* palette);    /*Load Doom’s color palette*/
+void vga_set_default_palette(void);    /*Restore standard VGA color palette*/
 
 #endif
